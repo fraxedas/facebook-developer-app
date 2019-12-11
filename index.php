7a99ddd47d1c2e7a35f4d72221a0d4608e6f0a9a
Ethan Goldman-Kirst <ethangk@fb.com>
2019-12-11 14:34:51 -0800

    add birthday

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
   <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=2469375149997342"></script>
 
   <!--TODO: Add Facebook Login button here-->
-  <div class="fb-login-button" data-onlogin="checkLoginState();" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
+  <!--Add `data-onlogin="checkLoginState();"` to trigger the JavaScript code below after login-->
+  <div class="fb-login-button" data-scope="user_birthday" data-onlogin="checkLoginState();" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
 
   <div id="status"></div>
   <br />
@@ -18,12 +19,17 @@
   <div id="birthday"</div>
 </body>
 <script>
+  // Below is JavaScript code to update the status on the page every time
+  // the page is loaded or the user finishes logging in
+
+  // Call this function after login is complete to check if the user is logged in
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
   }
 
+  // Changes the status on the page
   function statusChangeCallback(response) {
     if (response.status === 'connected') {
       testAPI();
@@ -32,15 +38,17 @@
     }
   }
 
+  // Query the Graph API and update the page with the user name
   function testAPI() {
     FB.api('/me', function(response) {
       document.getElementById('status').innerHTML = 'Logged in as ' + response.name;
+      document.getElementById('birthday').innerHTML = 'Your birthday is ' + response.birthday;
     });
   }
 
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : '2469375149997342',
+      appId      : '2469375149997342',       // TODO: replace with your App ID
       cookie     : true,                     // Enable cookies to allow the server to access the session.
       xfbml      : true,                     // Parse social plugins on this webpage.
       version    : 'v5.0'
@@ -48,7 +56,7 @@
 
 
     FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-      statusChangeCallback(response);        // Get the login status
+      statusChangeCallback(response);        // Get the login status and change the status
     });
   };
 </script>
