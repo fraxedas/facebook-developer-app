39236c5bf8bfcd77b04cc8d4ea9bf2c9895d6b26
Ethan Goldman-Kirst <ethangk@fb.com>
2019-12-11 14:06:26 -0800

    add Facebook Login dialog

--- index.php
+++ index.php
@@ -3,9 +3,53 @@
   <title>Website name goes here</title>
 </head>
 <body>
-  <!--TODO-->
+  <!--TODO: Add JavaScript SDK here-->
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=2469375149997342"></script>
+
+  <!--TODO: Add Facebook Login button here-->
+  <div class="fb-login-button" data-onlogin="checkLoginState();" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
+
+  <div id="status"></div>
+  <br />
+  <br />
+  <div id="name"></div>
+  <br />
+  <div id="birthday"</div>
 </body>
 <script>
-  // Add JavaScript here
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else {
+      document.getElementById('status').innerHTML = 'Please log into this webpage.';
+    }
+  }
+
+  function testAPI() {
+    FB.api('/me', function(response) {
+      document.getElementById('status').innerHTML = 'Logged in as ' + response.name;
+    });
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{app-id}',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : '{api-version}'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Get the login status
+    });
+  };
 </script>
 </html>
